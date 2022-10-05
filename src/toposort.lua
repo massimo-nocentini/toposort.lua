66af2b99a8686f14df2ba46bb3505637f173584c
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-10-05 09:17:08 +0200

    Extract function T

--- src/toposort.lua
+++ src/toposort.lua
@@ -6,37 +6,14 @@ local linkedstack = require 'linkedstack'
 
 local toposort = {}
 
-function toposort.sort(rel)
+local zero = 0
 
-	local pool = linkedpool.create ()
-	local count, top, n, zero = {}, {}, 0, 0
-
-	for requirement, dependants in pairs(rel) do
-
-		if not count[requirement] then
-			--print('requirement ' .. tostring(requirement))
-			count[requirement] = zero
-			top[requirement] = linkedstack.create (pool)
-			n = n + 1
-		end
-
-		for _, dependant in ipairs(dependants) do
-
-			if not count[dependant] then
-				--print('dependant ' .. tostring(dependant))
-				count[dependant] = zero
-				top[dependant] = linkedstack.create (pool)
-				n = n + 1
-			end
-
-			count[dependant] = count[dependant] + 1
-
-			top[requirement]:push (dependant)	-- record the relation
-		end
-	end
+local function T (n, count, top)
 
 	local qlink, first = {}, {}
-	local r = first
+
+	local r = first		-- this is a *very* important variable.
+
 	for k, c in pairs(count) do
 		if c == zero then 
 			qlink[r] = k
@@ -45,9 +22,8 @@ function toposort.sort(rel)
 	end
 
 	local order = {}
-	f = qlink[first]	-- `f` is the first value encountered  for which `count[k] == 0` holds.
 
-	--for k, v in pairs(qlink) do print(k, v) end
+	f = qlink[first]	-- `f` is the first value encountered  for which `count[k] == 0` holds.
 
 	while f do
 		table.insert(order, f)
@@ -68,6 +44,37 @@ function toposort.sort(rel)
 	assert (n == 0)
 
 	return order
+
+end
+
+function toposort.sort(rel)
+
+	local pool = linkedpool.create ()
+	local count, top, n = {}, {}, 0
+
+	for requirement, dependants in pairs(rel) do
+
+		if not count[requirement] then
+			count[requirement] = zero
+			top[requirement] = linkedstack.create (pool)
+			n = n + 1
+		end
+
+		for _, dependant in ipairs(dependants) do
+
+			if not count[dependant] then
+				count[dependant] = zero
+				top[dependant] = linkedstack.create (pool)
+				n = n + 1
+			end
+
+			count[dependant] = count[dependant] + 1
+
+			top[requirement]:push (dependant)	-- record the relation
+		end
+	end
+
+	return T (n, count, top)
 end
 
 
